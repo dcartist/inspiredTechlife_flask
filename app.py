b77705d714cc97c2e53dd8f10bffe5aca84431ce
DCartist <pbannerman13@gmail.com>
2020-12-24 11:23:58 -0500

    checking to see if env works

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import render_template
+from decouple import config
 
+API_USERNAME = config('USER')
+API_KEY = config('KEY')
+print(API_KEY)
 app = Flask(__name__)
 
 
@@ -10,6 +14,9 @@ def hello_world():
 @app.route("/about")
 def aboutSection():
     return render_template("about.html")
+@app.route("/trial")
+def trial():
+    return (f"{API_KEY}")
 
 
 # app.run(port=9000, debug=True)
